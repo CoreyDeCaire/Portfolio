7e42b8aac0c35df7515896817b4d8f487d38fdd6
CoreyDeCaire <coreydecaire19@gmail.com>
2022-08-02 22:19:13 -0400

    added CSS stylings to skills, work and header sections

---
+++ script.js
@@ -0,0 +1,21 @@
+const portfolioApp = {};
+
+const textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml9 .letter',
+        scale: [0, 1],
+        duration: 1000,
+        elasticity: 600,
+        delay: (el, i) => 35 * (i + 1)
+    }).add({
+        targets: '.ml9',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 5000
+    });
+
+portfolioApp.init();
