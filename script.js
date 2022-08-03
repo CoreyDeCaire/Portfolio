e337b5547019a689ed7500d256fd07324047ab1a
CoreyDeCaire <coreydecaire19@gmail.com>
2022-08-02 23:25:50 -0400

    additional css stylings and some JS added

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ anime.timeline({ loop: true })
         opacity: 0,
         duration: 1000,
         easing: "easeOutExpo",
-        delay: 5000
+        delay: 10000
     });
 
 portfolioApp.init();
