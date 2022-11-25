14b88dd95a6bfa8d871a69d8eedd042ad739528f
CoreyDeCaire <coreydecaire19@gmail.com>
2022-11-25 18:34:33 -0500

    added Css stylings and media queries to site

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const portfolioApp = {};
+// const portfolioApp = {};
 
-const textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// const textWrapper = document.querySelector('.ml9 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({ loop: true })
-    .add({
-        targets: '.ml9 .letter',
-        scale: [0, 1],
-        duration: 1000,
-        elasticity: 600,
-        delay: (el, i) => 35 * (i + 1)
-    }).add({
-        targets: '.ml9',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 10000
-    });
+// anime.timeline({ loop: true })
+//     .add({
+//         targets: '.ml9 .letter',
+//         scale: [0, 1],
+//         duration: 1000,
+//         elasticity: 600,
+//         delay: (el, i) => 35 * (i + 1)
+//     }).add({
+//         targets: '.ml9',
+//         opacity: 0,
+//         duration: 1000,
+//         easing: "easeOutExpo",
+//         delay: 10000
+//     });
+
+
 
 // portfolioApp.init();
